fab37abec88d3fd3bc09caf348f2e3a77fd45666
NathanCso <109382054+NathanCso@users.noreply.github.com>
2023-03-04 10:50:12 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,14 +33,14 @@ export default function Home() {
       <main className=' bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40'>
         <section className=' min-h-screen' >
          <nav className=' py-10 mb-12 flex justify-between'>
-          <h1 className=' text-xl font-burtons'>Portfolio</h1>
+          <h1 className='  text-sm my-3 font-burtons'>Portfolio</h1>
            <ul className=' flex items-center'>
             <li><BsFillMoonStarsFill
             onClick={() => setDarkMode (!darkMode)}
-             className=' cursor-pointer text-2xl' />
+             className=' cursor-pointer text-1xl' />
             </li>
             <li>
-              <a className='bg-gradient-to-r from-green-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8' 
+              <a className='bg-gradient-to-r from-green-500 text- to-teal-500 text-white px-2 py-1 border-none rounded-md ml-8' 
             href='#'> Click to the moon ! </a></li>
            </ul>
 
